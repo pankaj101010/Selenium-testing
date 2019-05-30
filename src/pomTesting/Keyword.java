9f0220a925b8763ef36c187f336180492bacdc38
pankaj101010 <48236067+pankaj101010@users.noreply.github.com>
2019-05-30 15:18:11 +0530

    Add files via upload

--- src/pomTesting/Keyword.java
+++ src/pomTesting/Keyword.java
@@ -1,16 +1,54 @@
 package pomTesting;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.TestListenerAdapter;
 
-public class Keyword {
-	public static WebDriver driver;
+public  class Keyword{
+	public  WebDriver driver;	
 
-	public static void openBrowser() {
+	private WebElement getValue(String locator, String value) {
+		WebElement link = null;
+		switch (locator) {
+		case "xpath":
+			link = driver.findElement(By.xpath(value));
+			break;
+		case "id":
+			link = driver.findElement(By.id(value));
+			break;
+		}
+		return link;
+
+	}
+
+	public  void openBrowser() {
 		driver = new ChromeDriver();
 
 	}
-   public static void openUrl() {
-	   driver.get("http://automationpractice.com/index.php");
-   }
+    
+	public WebDriver Rdriver() {
+		return driver;
+		
+	} 
+	public  void openUrl(String url) {
+		driver.get(url);
+	}
+	public void implicitWait(int second) {
+		driver.manage().timeouts().implicitlyWait(second, TimeUnit.SECONDS);
+	}
+	
+     
+	public void sendElement(String locator , String value,String sendkeys) {
+		getValue(locator, value).sendKeys(sendkeys);
+	}
+	public void onClick(String locator , String value)
+	{
+		getValue(locator, value).click();
+	}
+	
+	
 }
